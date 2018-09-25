d57d9705df28147bd606aff35c843197fcd02db7
Shane Long <shalong@gmail.com>
2018-09-25 16:19:37 -0400

    Initial Commit

---
+++ include/membersite_config.php
@@ -0,0 +1,39 @@
+<?PHP
+require_once("./include/fg_membersite.php");
+
+$fgmembersite = new FGMembersite();
+
+//Provide your site name here
+$fgmembersite->SetWebsiteName('EmoryCAMP.org');
+
+//Provide the email address where you want to get notifications
+$fgmembersite->SetAdminEmail('emorycamp@emory.edu');
+
+//Provide your database login details here:
+//hostname, user name, password, database name and table name
+//note that the script will create the table (for example, fgusers in this case)
+//by itself on submitting register.php for the first time
+$fgmembersite->InitDB(/*hostname*/'localhost',
+                      /*username*/'emorycam_db_usr',
+                      /*password*/'emoryCAMP2015!',					  
+                      /*database name*/'emorycam_main',
+                      /*table name*/'admin_users');
+
+//For better security. Get a random string from this link: http://tinyurl.com/randstr
+// and put it here
+$fgmembersite->SetRandomKey('qSRcVS6DrTzrPvr');
+
+$directory='files';
+
+?>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-77816556-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
